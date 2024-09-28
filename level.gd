dd91975557137b69828fe0a296f91e6299a3fb87
Wesley Chen <wesleyc1223@gmail.com>
2024-09-28 13:49:43 -0700

    add overlays

--- level.gd
+++ level.gd
@@ -10,10 +10,10 @@ func _process(delta):
 	if control.player1 == true:
 		$Player2Walls.visible = false
 		$Player1Walls.visible = true
-		$"../player2".visible = false
-		$"../player1".visible = true
+		$"../player2/MainSprite".visible = false
+		$"../player1/MainSprite".visible = true
 	else:
 		$Player2Walls.visible = true
 		$Player1Walls.visible = false
-		$"../player1".visible = false
-		$"../player2".visible = true
+		$"../player1/MainSprite".visible = false
+		$"../player2/MainSprite".visible = true
