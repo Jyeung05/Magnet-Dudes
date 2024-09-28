a4f303eddda304f198fbd6881861178425ac3fdb
Wesley Chen <wesleyc1223@gmail.com>
2024-09-27 20:35:31 -0700

    added player vanish when chaing dimensions

--- level.gd
+++ level.gd
@@ -9,7 +9,11 @@ func _ready():
 func _process(delta):
 	if control.player1 == true:
 		$Player2Walls.visible = false
-		$Player1Walls.visible = true;
+		$Player1Walls.visible = true
+		$"../player2".visible = false
+		$"../player1".visible = true
 	else:
 		$Player2Walls.visible = true
 		$Player1Walls.visible = false
+		$"../player1".visible = false
+		$"../player2".visible = true
