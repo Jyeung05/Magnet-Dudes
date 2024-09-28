caa215a88bc16637676104fd689dbf9cde0e8a07
Wesley Chen <wesleyc1223@gmail.com>
2024-09-28 14:45:00 -0700

    added UI for push(half done)

---
+++ scenes/push_wave_UI.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$".".scale.x-=0.005;
+	$".".scale.y-=0.005;
+	if $".".scale.x <0.1:
+		$".".scale.x = 1;
+		$".".scale.y = 1;
+
