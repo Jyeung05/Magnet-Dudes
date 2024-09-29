94c4878228f1a243b5c14226679fd3f0cd8e3c09
Wesley Chen <wesleyc1223@gmail.com>
2024-09-28 18:25:48 -0700

    magenet jump issue resolved?

--- magnet2.gd
+++ magnet2.gd
@@ -65,13 +65,14 @@ func magnet():
 	
 	for i in range(0,5):
 		if ray_casts[i].is_colliding():
-			var my_pos = get_global_position()
-			collision_point = ray_casts[i].get_collider()
-			if self.is_pull:
-				collision_point.pull(my_pos)
-			else:
-				
-				collision_point.push(my_pos)
+			if ray_casts[i].get_collider() != $"../../..":
+				var my_pos = get_global_position()
+				collision_point = ray_casts[i].get_collider()
+				if self.is_pull:
+					collision_point.pull(my_pos)
+				else:
+					
+					collision_point.push(my_pos)
 				
 		
 	#if ray_cast.self.is_colliding():
